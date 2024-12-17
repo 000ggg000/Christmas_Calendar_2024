44cd077991c15639b5bab63b0fb1af7f87d9ee39
goda <godasum@gmail.com>
2024-12-17 20:57:26 +0200

    day_17

--- day_17/script.js
+++ day_17/script.js
@@ -1,7 +1,6 @@
 "use strict";
 
-import { people } from './data.js';
-
+import { people } from "./data.js";
 
 const topThree = people.sort((a, b) => b.deedPoints - a.deedPoints).slice(0, 3);
 console.log(topThree);
